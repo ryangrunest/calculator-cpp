e6096a14d9b16182fe79a1470dc5e45681148f3c
Ryan Grunest <ryan.grunest@chipotle.com>
2024-09-12 21:29:15 -0700

    add output

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,28 +9,43 @@
 
 using namespace std;
 
-void main()
-{
-	char statement[100];
+// add return value
+int main() {
+  // removed unused variable statement[100];
 	int op1, op2;
 	char operation;
-	char answer="Y"
-	while (answer=='y')
-	{
-		cout << "Enter expression" <<endl;
-		cin >> op2 >> operation >> op1;
-		if (operation == "+");
-			cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
-		if (operation == '-');
-			cout >> op1 << " - " << op2 << " = " << op1 - op2 << endl;
-		if (operation == '*')
-			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl
-		if (operation == '/')
-			cout << op1 << " * " << op2 << " = " << op1 / op2 << endl;
+	char answer = 'Y'; // set correct parenthesis around chars
+
+	while (answer == 'Y' || answer == 'y') {  // add more readable spacing.
+		cout << "Enter expression" << endl; // fix endl spacing
+
+		cin >> op1 >> operation >> op2; // input variables in correct order
+    
+		if (operation == '+') { // set correct paren around chars, add more readable spacing, add curly brackets, remove semicolon
+			cout << op1 << " + " << op2 << " = " << op1 + op2 << endl; // fix << / >> lines
+    }
+
+		if (operation == '-') { // add curly brackets, remove semicolon
+			cout << op1 << " - " << op2 << " = " << op1 - op2 << endl; // fix << / >> lines
+    }
+
+		if (operation == '*') { // add curly brackets
+			cout << op1 << " * " << op2 << " = " << op1 * op2 << endl; // fix output string
+    }
+
+		if (operation == '/') { // add curly brackets
+			cout << op1 << " / " << op2 << " = " << op1 / op2 << endl; // fix output string
+    }
 
 		cout << "Do you wish to evaluate another expression? " << endl;
 		cin >> answer;
-	}
+	};
+
+  if (answer == 'N' || answer == 'n') {
+    cout << "Program Finished." << endl;
+  }
+  
+  return 0; // return expected output value
 }
 
 
