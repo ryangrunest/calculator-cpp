584821b4b71adbe9cbff32654f32d821099ba0db
Ryan Grunest <ryan.grunest@chipotle.com>
2024-09-12 21:07:52 -0700

    init

---
+++ Calculator.cpp
@@ -0,0 +1,40 @@
+/*
+ * Calculator.cpp
+ *
+ *  Date: 09/12/2024
+ *  Author: Ryan Grunest
+ */
+
+#include <iostream>
+
+using namespace std;
+
+void main()
+{
+	char statement[100];
+	int op1, op2;
+	char operation;
+	char answer="Y"
+	while (answer=='y')
+	{
+		cout << "Enter expression" <<endl;
+		cin >> op2 >> operation >> op1;
+		if (operation == "+");
+			cout << op1 << " + " << op2 >> " = " << op1 + op2 << endl;
+		if (operation == '-');
+			cout >> op1 << " - " << op2 << " = " << op1 - op2 << endl;
+		if (operation == '*')
+			cout << op1 << " / " << op2 << " = " << op1 * op2 << endl
+		if (operation == '/')
+			cout << op1 << " * " << op2 << " = " << op1 / op2 << endl;
+
+		cout << "Do you wish to evaluate another expression? " << endl;
+		cin >> answer;
+	}
+}
+
+
+
+
+
+
